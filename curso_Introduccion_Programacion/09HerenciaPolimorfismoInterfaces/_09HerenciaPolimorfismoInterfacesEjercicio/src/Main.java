30d6817a51d691bd6cb399ea9fde5c311e282e87
Franz George Muller Rueda <118325804+franzgeorgecode@users.noreply.github.com>
2023-02-03 12:00:53 -0500

    Ejercicio sesion 9 del bootcamp

---
+++ curso_Introduccion_Programacion/09HerenciaPolimorfismoInterfaces/_09HerenciaPolimorfismoInterfacesEjercicio/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+        cliente.edad= 30;
+        cliente.telefono=12345;
+        cliente.nombre="Geo";
+        cliente.credito=3000;
+        System.out.println("*************************************************** ");
+        System.out.println("Hola mi edad es : " + cliente.edad +" años" );
+        System.out.println("Me pueden llamar a este numero : " + cliente.telefono);
+        System.out.println("Me pueden decir : " + cliente.nombre);
+        System.out.println("Mi credito es : $ " + cliente.credito);
+        System.out.println("*************************************************** ");
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad= 25;
+        trabajador.telefono=67890;
+        trabajador.nombre="Luk";
+        trabajador.salario=1500;
+        System.out.println("*************************************************** ");
+        System.out.println("Hola mi edad es : " + trabajador.edad +" años" );
+        System.out.println("Me pueden llamar a este numero : " + trabajador.telefono);
+        System.out.println("Me pueden decir : " + trabajador.nombre);
+        System.out.println("Mi salario es : $ " + trabajador.salario);
+        System.out.println("*************************************************** ");
+    }
+}
+
+class Persona{
+    int edad;
+    String nombre;
+    int telefono;
+
+}
+
+class Cliente extends Persona{
+
+    int credito;
+
+}
+
+class Trabajador extends Persona{
+    float salario;
+}
